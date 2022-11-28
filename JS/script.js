7cb3ac9ed78ed039bb091eee22b4f3588f87404a
Andrey <javascriptandrey2022@gmail.com>
2022-11-28 14:46:50 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// alert('Close the window please!')
+
+// const result = confirm('A you here?')
+
+// console.log(result);
+
+// const answer = prompt("Your age 18?", "15");
+// console.log(answer +10);
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Сколько вам лет?', '');
+// answers[2] = prompt('как дела?', '');
+
+// document.write(answers);
+
+const category = 'toys';
+
+console.log(`https://sunurl.com/${category}`);
+
+const user = "Andrey";
+
+alert(`Привет ${user}`);
